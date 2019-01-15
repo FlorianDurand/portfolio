160d3a1b2b599afe9fed3aeb3ae2edd82761e656
FlorianDurand <contact@floriandurand.com>
2019-01-16 00:44:36 +0100

    Dynamic index.php

--- index.php
+++ index.php
@@ -1,124 +1,90 @@
-<?php get_header(); ?>   
-<!--- PAGE LOADING --->	
+<?php get_header(); ?>
+
 <div class="preloading fadeIn">
 
-	    <div class="container">
+<div class="container">
 
-		<!--- HERO HEADER --->
-	            <div class="row">
-	                <div class="col-md-12">
-                       <div class="hero-bg" >
-					        <h1 class="fade-anime-top"> JOHN DOE </h1>
-					        <h1 class="fade-anime-top"> I am a designer & Front-end developer. </h1>
-                            <h2 class="fade-anime-bottom"> I work with love. </h2>					
-                       </div>
-	                </div>
-				</div>
-		<!--- THE END HERO HEADER --->
+<!--- HERO HEADER --->
+        <div class="row">
+            <div class="col-md-12">
+                 <div class="hero-bg" >
+            <h1 class="fade-anime-top"> <?php the_field('name'); ?> </h1>
+            <h1 class="fade-anime-top"> <?php the_field('description'); ?> </h1>
+                      <h2 class="fade-anime-bottom"> <?php the_field('quote'); ?> </h2>					
+                 </div>
+            </div>
+  </div>
+<!--- THE END HERO HEADER --->
 
-		<!--- ABOUT --->
-	            <div class="row">
-	                <div class="col-md-6 col-xs-12">
-                       <div class="lg-col-bg" >
-					        <div class="lg-col-ttl2">About me</div>
-                            <div class="lg-col-wrt"> My name is John Doe. I am a designer and Front-end developer 
-							based in earth. My designs, solutions and ideas for your brands.<br><br>
-							<b>Capabilities</b><br>
-							Branding<br>
-							Illustration<br>
-							Web Design<br>
-							Web Development
-							</div>							
-                       </div>
-	                </div>
+<!--- ABOUT --->
+<?php if( have_rows('about') ): 
 
-					<div class="col-md-6 col-xs-12">
-                       <div class="lg-col-bg2" >
-                               <img src="img/covers/portrait.jpg" alt="" class="img-lg-col2">					   
-                       </div>
-	                </div>
-					
-				</div>
-		<!--- THE END ABOUT CLIENT --->
-
-		<!--- HERO HEADER WORK SLIDER --->
-                <div class="row">				
-				    <div class="col-md-12 xs-col-12">
-				       <div class="lg-col-bg" >
-                            <div class="lg-col-wrt2">
-							Case study coming soon to all works. See more designs on 
-							<a target="_blank" class="soc-lnk" href="http://dribbble.com/">Dribbble</a>, 
-							<a target="_blank" class="soc-lnk" href="http://instagram.com/">Instagram</a> or 
-							<a target="_blank" class="soc-lnk" href="http://behance.com/">Behance</a>.
-							</div>
-                       </div>							
-				    </div>					
-			    </div>		        
-
-			    <div class="row">
-	                <div class="col-md-6 col-xs-12">
-                        <div class="prjct-bg" >					
-					            <a target="blank" href="https://unsplash.com/photos/BP3XOsSPlGA"> <img src="img/covers/1.jpg" alt="" class="img-prjct-wrk"> </a>
-					          <div class="prjct-wrt-left-wrk"> </div>
-							  <div class="shw-cs2"> Project Title </div>
-							  <div class="shw-cs4"> Branding, Identity </div>
-							  <div class="shw-cs"> Case study ( Coming soon ) </div>
-						</div>					
-	                </div>
-					
-	                <div class="col-md-6 col-xs-12">
-                        <div class="prjct-bg" >					
-					            <a target="blank" href="https://unsplash.com/photos/i_6Y2V81ceA"> <img src="img/covers/2.jpg" alt="" class="img-prjct-wrk"> </a>
-					          <div class="prjct-wrt-right-wrk"> </div>
-							  <div class="shw-cs2"> Project Title </div>
-							  <div class="shw-cs4"> Typography </div>
-							  <div class="shw-cs"> Case study ( Coming soon ) </div>
-						</div>
-	                </div>
+while( have_rows('about') ): the_row(); 
+?>
+  <div class="row">
+          <div class="col-md-6 col-xs-12">
+                <div class="lg-col-bg" >
+          <div class="lg-col-ttl2"><?php the_sub_field('title'); ?></div>
+                    <div class="lg-col-wrt"> <?php the_sub_field('content'); ?>
+      </div>							
                 </div>
+          </div>
 
-			    <div class="row">
-	                <div class="col-md-6 col-xs-12">
-                        <div class="prjct-bg" >					
-					            <a target="blank" href="https://unsplash.com/photos/aX6MnM_xvXo"> <img src="img/covers/3.jpg" alt="" class="img-prjct-wrk"> </a>
-					          <div class="prjct-wrt-left-wrk"> </div>
-							  <div class="shw-cs2"> Project Title </div>
-							  <div class="shw-cs4"> Illustration, Art Direction </div>
-							  <div class="shw-cs"> Case study ( Coming soon ) </div>
-						</div>					
-	                </div>
-					
-	                <div class="col-md-6 col-xs-12">
-                        <div class="prjct-bg" >					
-					            <a target="blank" href="https://unsplash.com/photos/qX9Ie7ieb1E"> <img src="img/covers/4.jpg" alt="" class="img-prjct-wrk"> </a>
-					          <div class="prjct-wrt-right-wrk"> </div>
-							  <div class="shw-cs2"> Project Title </div>
-							  <div class="shw-cs4"> Street Art </div>
-							  <div class="shw-cs"> Case study ( Coming soon ) </div>
-						</div>
-	                </div>
-                </div>	
+  <div class="col-md-6 col-xs-12">
+                <div class="lg-col-bg2" >
+                        <img src="<?php the_sub_field('picture'); ?>" alt="" class="img-lg-col2">					   
+                </div>
+          </div>
+  
+  </div>
+<?php endwhile; ?>
 
-			    <div class="row">
-	                <div class="col-md-6 col-xs-12">
-                        <div class="prjct-bg" >					
-					            <a target="blank" href="https://unsplash.com/photos/5E5N49RWtbA"> <img src="img/covers/5.jpg" alt="" class="img-prjct-wrk"> </a>
-					          <div class="prjct-wrt-left-wrk"> </div>
-							  <div class="shw-cs2"> Project Title </div>
-							  <div class="shw-cs4"> Art Direction </div>
-							  <div class="shw-cs"> Case study ( Coming soon ) </div>
-						</div>					
-	                </div>
-					
-	                <div class="col-md-6 col-xs-12">
-                        <div class="prjct-bg" >					
-					            <a target="blank" href="https://unsplash.com/photos/GCQaYDCQwgc"> <img src="img/covers/6.jpg" alt="" class="img-prjct-wrk"> </a>
-					          <div class="prjct-wrt-right-wrk"> </div>
-							  <div class="shw-cs2"> Project Title </div>
-							  <div class="shw-cs4"> Branding, Web Design, App. Design </div>
-							  <div class="shw-cs"> Case study ( Coming soon ) </div>
-						</div>
-	                </div>
-                </div>	
-                <?php get_footer(); ?>			
-        
+<?php endif; ?>
+      
+<!--- THE END ABOUT CLIENT --->
+
+<!--- HERO HEADER WORK SLIDER --->
+<?php if( have_rows('social_networks') ): 
+
+while( have_rows('social_networks') ): the_row(); 
+?>
+<div class="row">
+  <div class="col-md-12 xs-col-12">
+    <div class="lg-col-bg">
+      <div class="lg-col-ttl2"><?php the_sub_field('title'); ?></div>
+      <div class="lg-col-wrt2">
+        <?php the_sub_field('content'); ?>
+      </div>
+    </div>
+  </div>
+</div>
+<?php endwhile; ?>
+
+<?php endif; ?>
+<div class="row">
+<?php $project = array(
+            'post_type' => 'project',
+         );
+         $the_query = new WP_Query( $project );
+         if($the_query -> have_posts())
+         {
+             while($the_query -> have_posts())
+            {
+                 $the_query -> the_post();
+                 ?> 
+                  <div class="col-md-6 col-xs-12">
+                    <div class="prjct-bg">
+                      <a target="blank" href="<?php the_field('project_link'); ?>"> <img src="<?php the_field('project_image'); ?>" alt="" class="img-prjct-wrk">
+                      </a>
+                      <div class="prjct-wrt-left-wrk"> </div>
+                      <div class="shw-cs2"><?php the_field('project_title'); ?> </div>
+                      <div class="shw-cs4"><?php the_field('project_description'); ?> </div>
+                      <div class="shw-cs"> <?php the_field('project_more'); ?></div>
+                    </div>
+                  </div>
+    
+                  <?php }}else{
+            echo "no result";}
+         ?>
+          </div>	
+<?php get_footer(); ?>
